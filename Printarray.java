d58e68e28bc800a7a1f1844670a2a3f9ac1833ea
rekhapanneer <rekhapanneer333@gmail.com>
2026-08-31 21:24:43 +0530

    Add files via upload

---
+++ Printarray.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Printarray {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Array elements:");
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+
+        sc.close();
+    }
+}
